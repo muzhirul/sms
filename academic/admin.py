066af8fb31971645b67dc4896ca449d49ba55b59
unknown <muzhirulislam@gmail.com>
2023-07-23 15:30:00 +0600

    ci: modify admin file for session

--- academic/admin.py
+++ academic/admin.py
@@ -2,4 +2,13 @@ from django.contrib import admin
 from academic.models import *
 
 # Register your models here.
-admin.site.register(Session)
+class SessionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ("Session Information",{'fields':[('code','session','institution','status'),]})
+    ]
+    list_display = ['code','session','institution','status','created_at']
+    search_fields = ['code','session','institution']
+    save_on_top = True
+    list_per_page = 15
+
+admin.site.register(Session,SessionAdmin)
