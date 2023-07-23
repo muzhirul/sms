6fd0c7bd0bbd8eef057c85b03ab949bc553dfdc1
unknown <muzhirulislam@gmail.com>
2023-07-23 15:32:14 +0600

    feat: add new model section in academic app

--- academic/models.py
+++ academic/models.py
@@ -19,3 +19,18 @@ class Session(models.Model):
     def __str__(self):
         return self.session
 
+class Section(models.Model):
+    code = models.CharField(max_length=20,blank=True,null=True,verbose_name='Section Code')
+    section = models.IntegerField(blank=True,null=True,verbose_name='Section')
+    institution = models.ForeignKey(Institution,on_delete=models.SET_NULL,blank=True,null=True)
+    status = models.BooleanField(default=True)
+    created_by = UserForeignKey(auto_user_add=True, on_delete=models.SET_NULL,related_name='section_creator', editable=False, blank=True, null=True)
+    updated_by = UserForeignKey(auto_user=True, on_delete=models.SET_NULL, related_name='section_update_by', editable=False, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'ac_section'
+    
+    def __str__(self):
+        return self.section
