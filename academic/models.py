2b2aee7e89ef87244b48687cb8822a650330d5f0
unknown <muzhirulislam@gmail.com>
2023-07-23 15:21:04 +0600

    feat: add session model and register in admin

--- academic/models.py
+++ academic/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from institution.models import Institution
+from django_userforeignkey.models.fields import UserForeignKey
 
 # Create your models here.
+class Session(models.Model):
+    code = models.CharField(max_length=20,blank=True,null=True,verbose_name='Session Code')
+    session = models.IntegerField(blank=True,null=True,verbose_name='Session')
+    institution = models.ForeignKey(Institution,on_delete=models.SET_NULL,blank=True,null=True)
+    status = models.BooleanField(default=True)
+    created_by = UserForeignKey(auto_user_add=True, on_delete=models.SET_NULL,related_name='session_creator', editable=False, blank=True, null=True)
+    updated_by = UserForeignKey(auto_user=True, on_delete=models.SET_NULL, related_name='session_update_by', editable=False, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'ac_session'
+    
+    def __str__(self):
+        return self.session
+
