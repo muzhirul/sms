474c8a363f3149f0e397167781c995a2d5e545ff
unknown <muzhirulislam@gmail.com>
2023-07-23 16:00:21 +0600

    feat: add class model in academic app

--- academic/models.py
+++ academic/models.py
@@ -36,3 +36,21 @@ class Section(models.Model):
     
     def __str__(self):
         return self.section
+
+class Class(models.Model):
+    code = models.CharField(max_length=10,blank=True,null=True,verbose_name='Class Code')
+    name = models.CharField(max_length=50,blank=True,null=True, verbose_name='Class Name')
+    institution = models.ForeignKey(Institution,on_delete=models.SET_NULL,blank=True,null=True)
+    section = models.ManyToManyField(Section)
+    status = models.BooleanField(default=True)
+    created_by = UserForeignKey(auto_user_add=True, on_delete=models.SET_NULL,related_name='class_creator', editable=False, blank=True, null=True)
+    updated_by = UserForeignKey(auto_user=True, on_delete=models.SET_NULL, related_name='class_update_by', editable=False, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'ac_class'
+        verbose_name = '3. Class'
+
+    def __str__(self):
+        return self.name
