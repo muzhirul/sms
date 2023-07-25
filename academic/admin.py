6d87f082daed002d056633f0d517e87755303033
unknown <muzhirulislam@gmail.com>
2023-07-25 10:27:41 +0600

    feat: add class room model in academic app

--- academic/admin.py
+++ academic/admin.py
@@ -53,9 +53,22 @@ class ClassAdmin(admin.ModelAdmin):
 
     class Meta:
         model = Class
+    
+class ClassRoomAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ("Class Room Information",{"fields":[('code','building','location'),('start_date','end_date'),('room_no','institution','status')]})
+    ]
+    list_display = ['code','building','location','room_no','status']
+    search_fields = ['code','building','location','room_no','status']
+    save_on_top = True
+    list_per_page = 15
+
+    class Meta:
+        model = ClassRoom
 
 admin.site.register(Version,VersionAdmin)
 admin.site.register(Session,SessionAdmin)
 admin.site.register(Section,SectionAdmin)
 admin.site.register(Class,ClassAdmin)
 admin.site.register(Subject,SubjectAdmin)
+admin.site.register(ClassRoom,ClassRoomAdmin)
--- academic/models.py
+++ academic/models.py
@@ -93,3 +93,27 @@ class Class(models.Model):
 
     def __str__(self):
         return self.name
+    
+class ClassRoom(models.Model):
+    LOCATION_TYPE = (('GROUND','Ground Floor'),('1st','1st Floor'),('2nd','2nd Floor'))
+    code = models.CharField(max_length=20, blank=True,null=True,verbose_name='Room Code')
+    location = models.CharField(max_length=20, blank=True,null=True,choices=LOCATION_TYPE)
+    building = models.CharField(max_length=100, blank=True,null=True, verbose_name='Building Name')
+    room_no = models.CharField(max_length=10, blank=True, null=True, verbose_name='Room No.')
+    institution = models.ForeignKey(Institution,on_delete=models.SET_NULL,blank=True,null=True)
+    start_date = models.DateField(blank=True, null=True)
+    end_date = models.DateField(blank=True,null=True)
+    status = models.BooleanField(default=True)
+    created_by = UserForeignKey(auto_user_add=True, on_delete=models.SET_NULL,related_name='class_room_creator', editable=False, blank=True, null=True)
+    updated_by = UserForeignKey(auto_user=True, on_delete=models.SET_NULL, related_name='class_room_update_by', editable=False, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'ac_class_room'
+        verbose_name = '6. Class Room'
+
+    def __str__(self):
+        return self.room_no
+    
+
