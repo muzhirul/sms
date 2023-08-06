88bddabd401237690f4e1519850825804e97144c
unknown <muzhirulislam@gmail.com>
2023-08-06 09:49:52 +0600

    fix: change class name

--- academic/serializers.py
+++ academic/serializers.py
@@ -31,7 +31,7 @@ class SubjectSerializer(serializers.ModelSerializer):
 
 class ClassSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Class
+        model = ClassName
         # Exclude the specified fields from serialization
         exclude = ['status', 'created_by', 'updated_by', 'created_at', 'updated_at']
 
