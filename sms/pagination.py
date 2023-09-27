ef21d160528c9fc7e11635a3d826feac24b5124f
Md. Muzhirul Islam <43285@wgbd.com>
2023-09-27 15:29:06 +0600

    ci: duplicate page_size and page_number problem solve

--- sms/pagination.py
+++ sms/pagination.py
@@ -1,35 +1,13 @@
 from rest_framework import pagination
 from rest_framework.response import Response
-from urllib.parse import urlencode
+from urllib.parse import urlparse, parse_qs, urlencode, urlunparse
 
 class CustomPagination(pagination.PageNumberPagination):
     # page_size = 10  # Number of items per page
     page_size_query_param = 'page_size'
     page_query_param = 'page_number'
-    # max_page_size = 1000  # Maximum page size
+    max_page_size = 1000  # Maximum page size
 
-    def get_next_link(self):
-        if not self.page.has_next():
-            return None
-        page_number = self.page.next_page_number()
-        url = self.request.build_absolute_uri()
-        query_params = urlencode({
-            self.page_query_param: page_number,
-            self.page_size_query_param: self.page_size,
-        })
-        return f"{url}?{query_params}"
-
-    def get_previous_link(self):
-        if not self.page.has_previous():
-            return None
-        page_number = self.page.previous_page_number()
-        url = self.request.build_absolute_uri()
-        query_params = urlencode({
-            self.page_query_param: page_number,
-            self.page_size_query_param: self.page_size,
-        })
-        return f"{url}?{query_params}"
-    
     def get_paginated_response(self, data):
         next_url = self.get_next_link()
         previous_url = self.get_previous_link()
@@ -41,14 +19,37 @@ class CustomPagination(pagination.PageNumberPagination):
                 "count": self.page.paginator.count,
                 "next": next_url,
                 "previous": previous_url,
-                # "page_size": self.page.paginator.per_page,
-                # "page_number": self.page.number,
             },
             "data": data,
-            
         }
         return Response(response_data)
-    
+
+    def get_next_link(self):
+        if not self.page.has_next():
+            return None
+
+        page_number = self.page.next_page_number()
+        url = self.request.build_absolute_uri()
+        url = self.replace_query_param(url, self.page_query_param, page_number)
+        return url
+
+    def get_previous_link(self):
+        if not self.page.has_previous():
+            return None
+
+        page_number = self.page.previous_page_number()
+        url = self.request.build_absolute_uri()
+        url = self.replace_query_param(url, self.page_query_param, page_number)
+        return url
+
+    def replace_query_param(self, url, param_name, param_value):
+        parsed_url = urlparse(url)
+        query_dict = parse_qs(parsed_url.query)
+        query_dict[param_name] = [param_value]
+        new_query = urlencode(query_dict, doseq=True)
+        return urlunparse((parsed_url.scheme, parsed_url.netloc, parsed_url.path,
+                           parsed_url.params, new_query, parsed_url.fragment))
+
 class CustomLimitOffsetPagination(pagination.LimitOffsetPagination):
     default_limit = 10  # Number of items per page
     # max_limit = 1000  # Maximum page size
