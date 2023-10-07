66eeb5c12f9d37a5a003e4385cd167559d4269fb
Md. Muzhirul Islam <43285@wgbd.com>
2023-10-07 15:41:57 +0600

    feat:Designation create update list and delete api

--- staff/serializers.py
+++ staff/serializers.py
@@ -16,3 +16,19 @@ class DepartmentListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Department
         fields = ['id','name']
+        
+class DesignationSerializer(serializers.ModelSerializer):
+    created_username = serializers.ReadOnlyField(source='created_by.username')
+    updated_username = serializers.ReadOnlyField(source='created_by.username')
+    institution_name = serializers.ReadOnlyField(source='institution.name')
+    branch_name = serializers.ReadOnlyField(source='branch.name')
+    class Meta:
+        model = Designation
+        # Exclude the 'status' field and other fields you want to exclude
+        exclude = ['status','code']
+        # exclude = ['created_by', 'updated_by', 'created_at', 'updated_at']
+
+class DesignationListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Designation
+        fields = ['id','name']
--- staff/urls.py
+++ staff/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path,include
-from .views import (StaffDepartmentListCreate,StaffDepartmentList, DepartmentDetail,DepartmentDelete)
+from .views import (StaffDepartmentListCreate,StaffDepartmentList, DepartmentDetail,DepartmentDelete,
+                    StaffDesignationListCreate,StaffDesignationList,DesignationDetail,DesignationDelete)
 
 urlpatterns = [
+    # Department api
     path('api/department', StaffDepartmentListCreate.as_view(), name='staff-department-list-create'),
     path('api/department/list', StaffDepartmentList.as_view(), name='staff-department-list'),
     path('api/department/detail/<int:pk>', DepartmentDetail.as_view(), name='department-detail'),
     path('api/department/delete/<int:pk>', DepartmentDelete.as_view(), name='department-delete'),
+    # Designation API
+    path('api/designation', StaffDesignationListCreate.as_view(), name='staff-designation-list-create'),
+    path('api/designation/list', StaffDesignationList.as_view(), name='staff-designation-list'),
+    path('api/designation/detail/<int:pk>', DesignationDetail.as_view(), name='designation-detail'),
+    path('api/designation/delete/<int:pk>', DesignationDelete.as_view(), name='designation-delete'),
 ]
