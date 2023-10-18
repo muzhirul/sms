e9b97d60b7dfecbd7ba683d5c282517ae906f3ae
Md. Muzhirul Islam <43285@wgbd.com>
2023-10-18 14:35:14 +0600

    feat: add examname and examroutine serializer

--- exam/serializers.py
+++ exam/serializers.py
@@ -6,3 +6,15 @@ class GradeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Grade
         fields = ['id','name','start_mark','end_mark','point','sl_no']
+        
+class ExamNameSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = ExamName
+        exclude = ['status','institution','branch','created_by','updated_by','created_at','updated_at']
+        
+class ExamRoutineSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = ExamRoutine
+        exclude = ['status','institution','branch','created_by','updated_by','created_at','updated_at']
