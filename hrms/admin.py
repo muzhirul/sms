a6edb04b77e3b8115f2eaf1d85ccaedfc035dc21
Md. Muzhirul Islam <43285@wgbd.com>
2023-10-31 13:53:25 +0600

    feat: add new table for account bank

--- hrms/admin.py
+++ hrms/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+class AccountBankAdmin(admin.ModelAdmin):
+    list_display = ['name','status']
+    
+    class Meta:
+        model = AccountBank
+        
+admin.site.register(AccountBank,AccountBankAdmin)
