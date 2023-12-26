25c235b6961038b6cece1d45cfc9c7d5775e0db4
Md. Muzhirul Islam <43285@wgbd.com>
2023-12-26 17:00:08 +0600

    ci: add new column in fees type model

--- fees/models.py
+++ fees/models.py
@@ -16,6 +16,7 @@ class FeesType(models.Model):
     name = models.CharField(max_length=100, verbose_name='Fees Type Name',validators=[validate_alpha_chars_only])
     code = models.SlugField(max_length=255)
     description = models.TextField(blank=True,null=True)
+    is_active = models.BooleanField(default=True)
     status = models.BooleanField(default=True)
     institution = models.ForeignKey(Institution, on_delete=models.CASCADE, blank=True, null=True)
     branch = models.ForeignKey(Branch, on_delete=models.CASCADE, blank=True, null=True)
