a84c7ea76d6deb6287752876bf3f7a5d55036630
Md. Muzhirul Islam <43285@wgbd.com>
2023-12-28 09:11:08 +0600

    feat: add fees delete api

--- fees/urls.py
+++ fees/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     # For Fees Type
     path('api/fees-type', FeesTypeCreateList.as_view(), name='fees-type-create-list'),
     path('api/fees-type/detail/<int:pk>', FeesTypeDetail.as_view(), name='fees-type-detail'),
+    path('api/fees-type/delete/<int:pk>', FeesTypeDelete.as_view(), name='fees-type-delete'),
 ]
--- fees/views.py
+++ fees/views.py
@@ -150,4 +150,25 @@ class FeesTypeDetail(generics.RetrieveUpdateAPIView):
         except Exception as e:
             # Handle other exceptions
             return CustomResponse(code=status.HTTP_500_INTERNAL_SERVER_ERROR, message="An error occurred during the update", data=str(e))
- 
+
+class FeesTypeDelete(generics.UpdateAPIView):
+    queryset = FeesType.objects.all()
+    serializer_class = FeesTypeCreateSerializer
+    # Requires a valid JWT token for access
+    permission_classes = [permissions.IsAuthenticated]
+
+    def partial_update(self, request, *args, **kwargs):
+        '''Check user has permission to Delete start'''
+        permission_check = check_permission(self.request.user.id, 'Fees Type', 'delete')
+        if not permission_check:
+            return CustomResponse(code=status.HTTP_401_UNAUTHORIZED, message="Permission denied", data=None)
+        '''Check user has permission to Delete End'''
+
+        instance = self.get_object()
+        if not instance.status:
+            return CustomResponse(code=status.HTTP_400_BAD_REQUEST, message=f"Fees Type {instance.name} already Deleted", data=None)
+        # Update the "status" field to False
+        instance.status = False
+        instance.save()
+        # Customize the response format for successful update
+        return CustomResponse(code=status.HTTP_200_OK, message=f"Fees Type {instance.name} Delete successfully", data=None)
