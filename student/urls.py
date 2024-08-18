f31b657da454fd55dd4cf0b36afb0b48adcb1c0a
Muzhirul Islam <43285@wgbd.com>
2024-08-18 09:23:14 +0600

    add student short details api

--- student/urls.py
+++ student/urls.py
@@ -4,6 +4,7 @@ from .views import *
 
 urlpatterns = [
     path('api/student', StudentList.as_view(), name='student-list'),
+    path('api/short/list', StudentShortList.as_view(), name='student-short-list'),
     path('api/student/detail/<int:pk>', StudentDetail.as_view(), name='student-detail'),
     path('api/student/image/<int:pk>', StudentImageUpload.as_view(), name='student-image-upload'),
     path('api/guardian/image/<int:pk>', GuardianImageUpload.as_view(), name='guardian-image-upload'),
--- student/views.py
+++ student/views.py
@@ -12,6 +12,55 @@ from datetime import datetime
 from academic.models import ClassTeacher
 
 # Create your views here.
+class StudentShortList(generics.ListAPIView):
+    # queryset = Version.objects.filter(status=True).order_by('id')
+    serializer_class = StudentSortViewSerializer
+    # Requires a valid JWT token for access
+    permission_classes = [permissions.IsAuthenticated]
+    pagination_class = CustomPagination
+
+    def get_queryset(self):
+        queryset = Student.objects.filter(status=True).order_by('-id')
+        try:
+            institution_id = self.request.user.institution
+            branch_id = self.request.user.branch
+            # users = Authentication.objects.get(id=user_id)
+            if institution_id and branch_id:
+                queryset = queryset.filter(
+                    institution=institution_id, branch=branch_id, status=True).order_by('-id')
+            elif branch_id:
+                queryset = queryset.filter(
+                    branch=branch_id, status=True).order_by('-id')
+            elif institution_id:
+                queryset = queryset.filter(
+                    institution=institution_id, status=True).order_by('-id')
+            else:
+                queryset
+        except:
+            pass
+        return queryset
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            response_data = self.get_paginated_response(serializer.data).data
+        else:
+            serializer = self.get_serializer(queryset, many=True)
+            response_data = {
+                "code": 200,
+                "message": "Success",
+                "data": serializer.data,
+                "pagination": {
+                    "next": None,
+                    "previous": None,
+                    "count": queryset.count(),
+                },
+            }
+
+        return Response(response_data)
+
 class StudentList(generics.ListCreateAPIView):
     serializer_class = StudentViewSerializer
     permission_classes = [permissions.IsAuthenticated]  # Requires a valid JWT token for access
