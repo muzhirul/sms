1843bbd81952331538c073d58fb8ace7c327b68e
Muzhirul Islam <43285@wgbd.com>
2024-08-20 17:35:45 +0600

    modify salary setup list api

--- hrms/serializers.py
+++ hrms/serializers.py
@@ -72,6 +72,37 @@ class SalarySetupDtlViewSerializer(serializers.ModelSerializer):
     class Meta:
         model = SalarySetupDtl
         fields = ['id', 'payroll_ele','fixed_amt','formula','min_amt','max_amt','remarks','status']
+    
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+
+        # Original formula string
+        formula = representation.get('formula', '')
+
+        # Fetch all mappings from the PayrollElement table
+        formula_mapping = {
+            element.value: element.name
+            for element in PayrollElement.objects.all()
+        }
+
+        # Build the formula as a list of key-value pairs
+        formula_parts = formula.split()
+        formula_list = []
+        
+        for part in formula_parts:
+            if part in formula_mapping:
+                # Add the formula part as a key-value pair
+                formula_list.append({part: formula_mapping[part]})
+            # elif part == '*':
+            #     formula_list.append({part: "Multiply"})
+            # elif part.replace('.', '', 1).isdigit():
+            #     percentage = f"{float(part) * 100:.0f}%"
+            #     formula_list.append({part: percentage})
+
+        # Replace the formula representation with the new list format
+        representation['formula'] = formula_list
+
+        return representation
 
 class SalarySetupMstViewSerializer(serializers.ModelSerializer):
     salary_setup_dtl = SalarySetupDtlViewSerializer(many=True, required=False, read_only=True)
