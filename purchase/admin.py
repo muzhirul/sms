7f24c34de05cb28a8462041ae01e67aea9c0af0e
Muzhirul Islam <43285@wgbd.com>
2024-11-04 09:34:21 +0600

    modify supplier model

--- purchase/admin.py
+++ purchase/admin.py
@@ -6,6 +6,22 @@ from .models import *
 class SupplierAdmin(admin.ModelAdmin):
     list_display = ['name','short_name','address','owner_name','phone_number','email']
     search_fields = ['name','short_name','address','owner_name','phone_number','email']
+    fieldsets = [
+        ("Basic Information",{'fields':[('code','name','short_name'),
+                                        ('type','tin','bin'),
+                                        ('phone_number','email')]}),
+        ("Owner Information",{'fields':[('owner_name','owner_phone_number',),
+                                        ('owner_email','owner_nid') ]}),
+        ("Contact Person Information",{'fields':[('con_person','con_number','con_email')]}),
+        ("Financial Information",{'fields':[('bank','branch_name','account_number'),
+                                            ('opening_amt','current_amt')]}),
+        ("Contact Information",{'fields':[('thana','district','division'),
+                                          ('country','address')]}),
+        ("Other Information",{'fields':[('institution','branch','is_active','status'),
+                                        ]})
+
+        
+    ]
 
     class Meta:
         model = Supplier
